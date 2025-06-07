cc1089daa46e56f565e6ec629b1e72d60fed8691
go98zoh <go98zoh@mytum.de>
2025-06-07 15:47:44 +0200

    fixed bug

--- proto/gen/python/odin/v1/__init__.py
+++ proto/gen/python/odin/v1/__init__.py
@@ -7,19 +7,13 @@ Generated protobuf definitions for Odin API v1.
 from . import audio_pb2
 from . import common_pb2
 from . import options_pb2
-from . import session_pb2
-from . import speech_pb2
-from . import system_pb2
+from . import text_helper_pb2
 from . import text_pb2
-from . import tools_pb2
 
 __all__ = [
     "audio_pb2",
     "common_pb2",
     "options_pb2",
-    "session_pb2",
-    "speech_pb2",
-    "system_pb2",
+    "text_helper_pb2",
     "text_pb2",
-    "tools_pb2",
 ]
